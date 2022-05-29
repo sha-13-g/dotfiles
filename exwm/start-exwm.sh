e58d8f173dfec9607a5e8430def3697071acd8d2
Ganfina Brice <ganfinab@gmail.com>
2022-05-29 13:20:30 +0000

    Change Desktop entry to Exwm and add better start script

--- exwm/start-exwm.sh
+++ exwm/start-exwm.sh
@@ -1,8 +1,33 @@
 #! /bin/sh
 #xss-lock -- bash "~/lock.sh"
-setxkbmap -option 'grp:shifts_toggle, ctrl:swapcaps' -layout 'fr' -variant 'us-azerty' -model 'pc105'
-nitrogen --restore
+# -layout 'fr' -variant 'us-azerty' -model 'pc105' # 
+
+
+setxkbmap -option 'grp:shifts_toggle, ctrl:swapcaps'
+
+# Load compositor
 picom &
-exec dbus-launch --exit-with-session emacs --deamon --debug-init  
-exec sleep 3
-exec emacsclient -mm
+
+# Set nice pointer cursor.
+# xsetroot -cursor_name left_ptr &
+# unclutter &
+
+# Run dunst in background
+# dunst &
+
+# Load trayer and polybar
+# polybar &
+
+# trayer --edge top --align right --height 15 --width 5 --iconspacing 5 &
+
+nitrogen --restore
+
+# pa-applet &
+# nm-applet &
+
+exec dbus-launch --exit-with-session emacs -mm --debug-init
+
+# picom &
+# exec dbus-launch --exit-with-session emacs --deamon --debug-init  
+# exec sleep 3
+# exec emacsclient -mm
